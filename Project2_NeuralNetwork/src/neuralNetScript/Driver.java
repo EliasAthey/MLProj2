816aa77394e22e3fdd828985150ea10224bd4f43
Elias Athey <athey.elias@gmail.com>
2017-10-07 14:21:34 -0600

    implemented buildNetwork for the mlp case

--- Project2_NeuralNetwork/src/neuralNetScript/Driver.java
+++ Project2_NeuralNetwork/src/neuralNetScript/Driver.java
@@ -17,7 +17,7 @@ public class Driver {
 	private static int numOutNodes;
 	
 	// the network itself
-	private Layer[] network;
+	private static ArrayList<Layer> network;
 	
 	public static void main(String args[]){
 		// TODO
@@ -52,24 +52,75 @@ public class Driver {
 		for(int i = 0; i < input.size() - 1; i++){
 			output += Math.pow(1 - input.get(i), 2) + (100 * Math.pow(input.get(i + 1) - Math.pow(input.get(i), 2), 2));
 		}
-		
 		return output;
 	}
 	
 	// create Node objects and set downstream attribute for each
-	private static void buildNetwork(String networkType) throws Exception{
+	private static void buildNetwork() throws Exception{
 		// TODO
-		switch(networkType){
+		switch(Driver.networkType){
 			case "rbf":
 				// use k-value to create clusters via k-means clustering; this determines # of hidden nodes
 				// create output node
 				// create hidden nodes, set downstream to output, set each associatedCluster
 				// create input nodes, set the input, set downstream to all nodes in hidden layer
 				// set the sigma value for RadialBasisFunction to whatever we want
-				RadialBasisFunction.setSigma(2.5f);
+				RadialBasisFunction.setSigma(2.5);
 				break;
+				
 			case "mlp":
+				// create output node
+				// create hidden layers in reverse, setting the downstream nodes accordingly
+				// create input layer, set downstream nodes to first hidden layer
+				
+				// initialize Layers and network
+				Layer inputLayer = new Layer();
+				Layer[] hiddenLayers = new Layer[Driver.numHiddenLayers.size()];
+				for(int i = 0; i < hiddenLayers.length; i++){
+					hiddenLayers[i] = new Layer();
+				}
+				Layer outputLayer = new Layer();
+				Driver.network = new ArrayList<Layer>();
+				Driver.network.add(inputLayer);
+				for(Layer layer : hiddenLayers){
+					Driver.network.add(layer);
+				}
+				Driver.network.add(outputLayer);
+				
+				// create output nodes and store in output layer
+				Node[] outputNodes = new Node[Driver.numOutNodes];
+				for(int i = 0; i < outputNodes.length; i++){
+					// set the node functions for output nodes
+					outputNodes[i] = new Node(new PerceptronOutFunction(), new BackpropFinalWeightFunction(), new Node[0]);
+				}
+				outputLayer.setNodes(outputNodes);
+				
+				// create hidden layer nodes and store in hidden layer
+				Node[] prevHiddenNodes = null;
+				for(int i = hiddenLayers.length - 1; i >= 0; i--){
+					Node[] hiddenNodes = new Node[Driver.numHiddenLayers.get(i)];
+					for(int j = 0; j < hiddenNodes.length; j++){
+						// set the node functions for hidden nodes
+						if(i == hiddenLayers.length){
+							hiddenNodes[j] = new Node(new SigmoidalFunction(), new BackpropHiddenWeightFunction(), outputNodes);
+						}
+						else{
+							hiddenNodes[j] = new Node(new SigmoidalFunction(), new BackpropHiddenWeightFunction(), prevHiddenNodes);
+						}
+					}
+					hiddenLayers[i].setNodes(hiddenNodes);
+					prevHiddenNodes = hiddenNodes;
+				}
+				
+				// create input nodes and store in input layer
+				Node[] inputNodes = new Node[Driver.numInNodes];
+				for(int i = 0; i < inputNodes.length; i++){
+					// set the node functions for input nodes
+					inputNodes[i] = new Node(new SigmoidalFunction(), new NoWeightFunction(), prevHiddenNodes);
+				}
+				inputLayer.setNodes(inputNodes);
 				break;
+				
 			default: throw new Exception("The specified network type is not defined.");
 		}
 	}

20e658e9be6e22e2ab294f89ccd05c178fc44512
tiasmith <tiasmithr@gmail.com>
2017-10-11 18:23:53 -0600

    rbf, need to call rbf function method and pass right input to the setAssociatedCluster() method

--- MLProj2-dev/Project2_NeuralNetwork/src/neuralNetScript/RadialBasisFunction.java
+++ MLProj2-dev/Project2_NeuralNetwork/src/neuralNetScript/RadialBasisFunction.java
@@ -3,8 +3,7 @@
  */
 package neuralNetScript;
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.ListIterator;
+
 
 /**
  * @author Elias Athey, Tia Smith, Aaron McCarthy
@@ -14,33 +13,25 @@ class RadialBasisFunction implements INodeFunction {
 
 	private static double sigma;
 	private double[] associatedCluster;// one of the vectors made via k-means clustering
-	//ArrayList<Double> test = new ArrayList<Double>();
+	//ArrayList<Double> test = new ArrayList<Double>(); //where this is the input of datapoints
 	
 	@Override
 	public double computeOutput(double weightedSum) {
-		//need to call RBF here RadialBasisFunction.rbf(test);
+		//need to call RBF here this.rbf(test); //fix here
 		return 0;
 	}
 
 	// Radial Basis Function
-		private static double rbf(ArrayList<Double> input) throws Exception{
-			double current = -1;
-			double previous = -1;
+		private double rbf(ArrayList<Double> input) throws Exception{
+			double output = -1;
 			if(input.size() < 1){
 				throw new Exception("The Radial Basis Function needs at least one input");
 			}
-			double output = -1;
-			ListIterator<Double> iter = input.listIterator();
-			while(iter.hasNext()) {
-				current = iter.next();
-			}
-			while(iter.hasPrevious()) {
-				previous = iter.previous();
-			}
 			for(int i = 0; i < input.size() - 1; i++){
-				double center = (current- previous); //finds the center of the cluster
-				double variance = 2 * Math.pow(sigma, 2);
-				output = Math.exp(-1 * ((Math.pow(center, 2))) / variance);			}
+				double top = (input.get(i)- this.setAssociatedCluster(input.get(i)));  //fix here
+				double variance = 2 * Math.pow(sigma, 2); 
+				output = Math.exp(-1 * ((Math.pow(top, 2))) / variance);			
+			}
 			return output;
 		}
 		
@@ -49,7 +40,7 @@ class RadialBasisFunction implements INodeFunction {
 		RadialBasisFunction.sigma = sigma;
 	}
 	
-	// sets the associated cluster vector
+	// sets the associated cluster vector  
 	public void setAssociatedCluster(double[] cluster){
 		this.associatedCluster = cluster;
 	}
